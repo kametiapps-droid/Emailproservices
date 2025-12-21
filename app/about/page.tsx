cc3739c1637584a1038b8b51a010ad0e9d40ca66
kametiapps-droid <kametiapps@gmail.com>
2025-12-21 15:34:39 +0400

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -28,7 +28,7 @@ export default function AboutPage() {
             <li style={{ marginBottom: '12px' }}>Protect your primary email from spam</li>
             <li style={{ marginBottom: '12px' }}>Maintain your privacy online</li>
             <li style={{ marginBottom: '12px' }}>Use an alternative email for newsletters, trials, and testing purposes</li>
-            <li style={{ marginBottom: '12px' }}Receive automated messages for testing and privacy protection only</li>
+            <li style={{ marginBottom: '12px' }}>Receive automated messages for testing and privacy protection only</li>
             <li style={{ marginBottom: '12px' }}>Test email functionality</li>
           </ul>
 
