f9a968531ba976589216d33dc85fbe35a42fff65
kametiapps-droid <kametiapps@gmail.com>
2025-12-25 23:39:53 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -256,6 +256,16 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Google Analytics */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SP2GJ092X1" />
+
+<Script id="gtag-init" strategy="afterInteractive">
+{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-SP2GJ092X1');
+`}
+</Script>
         
         <GoogleAnalytics />
         <meta charSet="utf-8" />
