3a6302f542f6f4fb179c293959e1f7f07833c3bf
kametiapps-droid <kametiapps@gmail.com>
2025-12-26 00:25:00 +0400

    Update GoogleAnalytics.tsx

--- app/GoogleAnalytics.tsx
+++ app/GoogleAnalytics.tsx
@@ -1,17 +1,27 @@
+'use client';
+
+import Script from 'next/script';
+
 export default function GoogleAnalytics() {
   return (
     <>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-SP2GJ092X1"></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-SP2GJ092X1');
-          `,
-        }}
+      {/* Load the Google Analytics script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-SP2GJ092X1`}
       />
+      
+      {/* Configure Google Analytics */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-SP2GJ092X1', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
     </>
   );
 }
